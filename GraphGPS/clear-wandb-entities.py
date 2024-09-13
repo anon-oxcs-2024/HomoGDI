de9c47355c7dbe086823405d9f2c2651dfa339c5
AnonUDL2024 <anonymous.udl.2024@gmail.com>
2024-09-13 00:56:16 -0400

    more setup

--- GraphGPS/clear-wandb-entities.py
+++ GraphGPS/clear-wandb-entities.py
@@ -1,14 +1,21 @@
 import os
 
 # Define the directory containing the .yaml files
-directory_path = '/Users/linusbao123/Documents/Oxford/Thesis/Graph ML/repo_submit/efficient attempt git/Homomorphism-GRL/GraphGPS/configs/GPS'
+directory_path = '/Users/linusbao123/Documents/Oxford/Thesis/Graph ML/repo_submit/efficient attempt git/Homomorphism-GRL/GraphGPS/cleaning-test'
 
 # Define the line to be deleted (exact match)
 line_to_delete = '  entity: linusbao'
 
+#recurisvely get all files in directory
+cfgs=[]
+for root, dirs, files in os.walk(directory_path):
+    for file in files:
+        cfgs.append(os.path.abspath(os.path.join(root, file)))
+
 # Iterate over each file in the directory
-for filename in os.listdir(directory_path):
+for filename in cfgs:
     if filename.endswith('.yaml'):
+        
         file_path = os.path.join(directory_path, filename)
         
         # Read the file
