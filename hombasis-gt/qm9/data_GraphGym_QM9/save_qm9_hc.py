a1a5149260a397e514ae0d3b60e60146568d3ee2
AnonUDL2024 <anonymous.udl.2024@gmail.com>
2024-09-12 22:17:56 -0400

    get rid of abs paths in QM9 processing py

--- hombasis-gt/qm9/data_GraphGym_QM9/save_qm9_hc.py
+++ hombasis-gt/qm9/data_GraphGym_QM9/save_qm9_hc.py
@@ -3,20 +3,21 @@ import torch_geometric
 from torch_geometric.data import InMemoryDataset, Data
 import os.path as osp
 
+from pathlib import Path
 import sys
-sys.path.append('/data/coml-graphml/kell7068/thesis_code_1/hombasis-gt/qm9/src/utils')
+import os
+
+main_dir = Path(__file__).parent.parent.parent.parent
+print(main_dir)
+sys.path.append(os.path.join(main_dir, 'hombasis-gt','qm9','src','utils'))
 import dataset_loader as dl
 from shortest_paths import ShortestPathTransform
 
-# sys.path.append('/data/coml-graphml/kell7068/thesis_code_1/GraphGPS/graphgps/loader')
-# from master_loader import join_dataset_splits
-
 import time
-
-sys.path.append('/data/coml-graphml/kell7068/thesis_code_1/hombasis-gt/qm9/data_GraphGym_QM9')
+sys.path.append(os.path.join(main_dir, 'hombasis-gt','qm9','data_GraphGym_QM9'))
 from CustomDataset import CustomDataset
 
-#copied from /data/coml-graphml/kell7068/thesis_code_1/GraphGPS/graphgps/loader/master_loader.py to avert double register
+#copied from GraphGPS/graphgps/loader/master_loader.py to avert double register
 def join_dataset_splits(datasets):
     """Join train, val, test datasets into one dataset object.
 
@@ -44,21 +45,9 @@ def join_dataset_splits(datasets):
 
     return datasets[0]
 
-# class CustomDataset(InMemoryDataset):
-#     def __init__(self,listOfDataObjects, root=None):
-#         super().__init__(root=root)
-#         self.data, self.slices = self.collate(listOfDataObjects)
-    
-#     def __len__(self):
-#         return len(self.slices)
-    
-#     def __getitem__(self, idx):
-#         sample = self.get(idx)
-#         return sample
-
 #takes in three lists of pyg elements and returns a list of three pyg InMemoryDatasets
 def get_qm9_datasets(tr_graphs, val_graphs, tst_graphs):
-    data_dir = "/data/coml-graphml/kell7068/thesis_code_1/hombasis-gt/qm9/data_GraphGym_QM9/storage"
+    data_dir = f"{main_dir}/hombasis-gt/qm9/data_GraphGym_QM9/storage"
     tr_graphs = CustomDataset(tr_graphs, root=data_dir)
     val_graphs = CustomDataset(val_graphs, root=data_dir)
     tst_graphs = CustomDataset(tst_graphs, root=data_dir)
@@ -87,59 +76,21 @@ def get_qm9_graphHC_lists(root_dir, include_shortest_path=True):
     return tr_graphs, val_graphs, tst_graphs, num_feat, num_pred
 
 start_t = time.time()
-tr_graphs, val_graphs, tst_graphs, num_feat, num_pred = get_qm9_graphHC_lists('/data/coml-graphml/kell7068/thesis_code_1/hombasis-gt/qm9')
+tr_graphs, val_graphs, tst_graphs, num_feat, num_pred = get_qm9_graphHC_lists(f'{main_dir}/hombasis-gt/qm9')
 get_qm9_graphHC_lists_t = time.time()
-print(f'len tr_graphs: {len(tr_graphs)}')
-print(f'len val_graphs: {len(val_graphs)}')
-print(f'len tst_graphs: {len(tst_graphs)}')
-#print(f'tr_graphs list: {tr_graphs}')
-print(f'tr_graphs list [0]:{tr_graphs[0]}')
+
 dataset_lists = get_qm9_datasets(tr_graphs, val_graphs, tst_graphs)
 get_qm9_datasets_t = time.time()
-print(f'len tr_dataset: {len(dataset_lists[0])}')
-print(f'dataset_lists: {dataset_lists}')
-print(f'dataset_lists[0]: {dataset_lists[0]}')
-print(f'dataset_lists[0][0]: {dataset_lists[0][0]}')
+
 joined_dataset = join_dataset_splits([dataset_lists[0], dataset_lists[1], dataset_lists[2]])
 joined_dataset_t = time.time()
-print(f'dataset slices: {joined_dataset.slices}')
-print(f'dataset data: {joined_dataset.data}')
+
 
 #UNCOMMENT BELOW TO SAVE GRAPH_HC
-torch.save(joined_dataset,"/data/coml-graphml/kell7068/thesis_code_1/datasets/QM9-GraphHC/processed/joined.pt")
+save_in_dir=f"{main_dir}/datasets/QM9-GraphHC/processed"
+if not os.path.exists(save_in_dir):
+    os.makedirs(save_in_dir)
+torch.save(joined_dataset, os.path.join(save_in_dir,"joined.pt"))
 save_t = time.time()
 
-print(f'joined dataset: {joined_dataset}')
-print(f'joineddataset[0]: {joined_dataset[0]}')
-print(f'time to complete get_qm9_graphHC_lists: {get_qm9_graphHC_lists_t-start_t}')
-print(f'time to complete get_qm9_datasets: {get_qm9_datasets_t-get_qm9_graphHC_lists_t}')
-print(F'time to complete joining the datasets: {joined_dataset_t-get_qm9_datasets_t}')
-print(f'time to save: {save_t-joined_dataset_t}')
 print(f'total time: {save_t-start_t}')
-print('(time unit is seconds I believe)')
-
-# def preformat_ZINC(dataset_dir, name, postfix=None):
-#     """Load and preformat ZINC datasets.
-
-#     Args:
-#         dataset_dir: path where to store the cached dataset
-#         name: select 'subset' or 'full' version of ZINC
-
-#     Returns:
-#         PyG dataset object
-#     """
-#     data_dir = "/data/coml-graphml/kell7068/thesis_code_1/hombasis-gt/hombasis-bench/data/zinc-data"
-#     dataset = None
-#     if name not in ['subset', 'full']:
-#         raise ValueError(f"Unexpected subset choice for ZINC dataset: {name}")
-#     dataset = join_dataset_splits(
-#         [ZINC(root=dataset_dir, subset=(name == 'subset'), split=split)
-#         for split in ['train', 'val', 'test']]
-#     )
-#     if postfix != None and "Spasm" in postfix and name == 'subset':
-#         count_files = ['zinc_with_homs_c7.json', 'zinc_with_homs_c8.json']
-#         idx_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 , 11, 15, 20, 21, 22, 24, 25, 27, 29, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46]
-#         sub_file = 'zinc_3to8C_multhom.json'
-#         dataset = get_data.add_zinc_subhom(name='ZINC', hom_files=count_files, idx_list=idx_list, sub_file=sub_file, root=data_dir, dataset=dataset)
-
-#     return dataset
